a85740bc98f67308966579030195328a7ea1b5ef
Luiz Mendes <luiz85mendes@gmail.com>
2018-05-30 16:20:19 -0300

    primeiro commit backend

---
+++ src/main/java/br/com/ascary/AscaryApplication.java
@@ -0,0 +1,12 @@
+package br.com.ascary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscaryApplication.class, args);
+	}
+}
