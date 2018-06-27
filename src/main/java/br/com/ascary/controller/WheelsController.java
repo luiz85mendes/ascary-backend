edac7fdf95b485e85b8f2e917a7847afb17e6efa
Luiz Mendes <luiz.mendes@squadra.com.br>
2018-06-27 09:56:55 -0300

    crossorigin annotation

--- src/main/java/br/com/ascary/controller/WheelsController.java
+++ src/main/java/br/com/ascary/controller/WheelsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.ascary.model.Wheels;
 import br.com.ascary.service.WheelsService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/wheels", produces = MediaType.APPLICATION_JSON_VALUE ) 
 public class WheelsController {
